e754081414a78ec8d02c97eadcd6b8c813ef6b34
hyojae-yang <aksghkrk15@naver.com>
2025-07-07 14:50:23 +0900

    20250707
    gd

--- Assets/2.Script/BulletSpawner.cs
+++ Assets/2.Script/BulletSpawner.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public GameObject[] BulletPrefab;
+    public float spawnRatemin = 0.5f;
+    public float spawnRatemax = 1.5f;
+    private float spawnRate;
+    private float timeAfterSpawn;
+    Transform target;
+    GameManager gameManager;
+    private void Awake()
     {
-        
+        gameManager = FindFirstObjectByType<GameManager>();
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRatemin, spawnRatemax);
+        target = FindFirstObjectByType<PlayerController>().transform;
+    }
+    private void Update()
+    {
+            timeAfterSpawn += Time.deltaTime;
+            if (timeAfterSpawn >= spawnRate)
+            {
+                timeAfterSpawn = 0f;
+                spawnRate = Random.Range(spawnRatemin, spawnRatemax);
+
+                GameObject bullet = Instantiate(BulletPrefab[Random.Range(0, BulletPrefab.Length)], transform.position, transform.rotation);
+                bullet.transform.LookAt(target);
+            }
     }
 }
--- Assets/2.Script/PlayerController.cs
+++ Assets/2.Script/PlayerController.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
-
+using System.Collections;
 public class PlayerController : MonoBehaviour
 {
     public float Speed = 5f;
     public int Hp = 3;
     public Rigidbody Rb;
+    private bool isInvincible = false;
     private void Start()
     {
         Rb = GetComponent<Rigidbody>();
@@ -25,5 +26,62 @@ public class PlayerController : MonoBehaviour
         Vector3 direction = new Vector3(xSpeed, 0, zSpeed);
         Rb.linearVelocity = direction;
 
+    }
+    public void Die(GameObject bulletTag)
+    {
+        if (isInvincible)
+        {
+            return;
+        }
+        GameManager gameManager = FindAnyObjectByType<GameManager>();
+        if (bulletTag.gameObject.tag == "Dibuff")
+        {
+            StartCoroutine(DibuffSlow());
+            return;
+        }
+        Hp--;
+        gameManager.HealthUpdate(Hp);
+        if (Hp == 0)
+        {
+            gameManager.EndGame();
+            gameObject.SetActive(false);
+        }
+    }
+    IEnumerator DibuffSlow()
+    {
+        float originSpeed = Speed;
+        Speed /= 2;
+        yield return new WaitForSeconds(3f);
+        Speed = originSpeed;
+    }
+    public void BuffHp()
+    {
+        if (Hp < 6)
+        { Hp++; }
+        GameManager gameManager = FindAnyObjectByType<GameManager>();
+        gameManager.HealthUpdate(Hp);
+    }
+    public void BuffSpeed(int speedpoint)
+    {
+        Speed += speedpoint;
+    }
+    public void BuffisInvincible()
+    {
+        Transform buffEffect = transform.Find("Invincuble");
+        if (buffEffect != null)
+        {
+            buffEffect.gameObject.SetActive(true);
+        }
+        isInvincible = true;
+        Invoke("UnBuffisInvincible", 3f);
+    }
+    public void UnBuffisInvincible()
+    {
+        Transform buffEffect = transform.Find("Invincuble");
+        if (buffEffect != null)
+        {
+            buffEffect.gameObject.SetActive(false);
+        }
+        isInvincible = false;
     }
 }
